aed04dc32083c3fa0892b573789e6f332d3b4fbd
Ivan Diaz <ivanid22@gmail.com>
2020-02-20 18:17:22 -0300

    Fix linter issues

--- main.rb
+++ main.rb
@@ -1,42 +1,29 @@
+# frozen_string_literal: true
 
-def bubble_sort(array_to_sort = [])
-  
-  return false if !array_to_sort 
+def bubble_sort(arr)
+  arr.each_with_index do |_, i|
+    arr.each_with_index do |_, j|
+      next if j >= arr.length - 1 - i
 
-  for i in (0...array_to_sort.length)
-    swapped = false
-    for j in (0...array_to_sort.length-i-1)
-      if array_to_sort[j] > array_to_sort[j+1]
-        array_to_sort[j], array_to_sort[j+1] =  array_to_sort[j+1], array_to_sort[j]
-        swapped = true
-      end
+      arr[j], arr[j + 1] = arr[j + 1], arr[j] if arr[j] > arr[j + 1]
     end
-    break if !swapped
   end
-
-  return array_to_sort
+  arr
 end
 
-def bubble_sort_by(array_to_sort = [])
+def bubble_sort_by(arr = [])
+  arr.each_with_index do |_, i|
+    arr.each_with_index do |_, j|
+      next if j >= arr.length - 1 - i
 
-  return false if !array_to_sort 
-
-  for i in (0...array_to_sort.length)
-    swapped = false
-    for j in (0...array_to_sort.length-i-1)
-      if yield(array_to_sort[j], array_to_sort[j+1]) == 1
-        array_to_sort[j], array_to_sort[j+1] =  array_to_sort[j+1], array_to_sort[j]
-        swapped = true
-      end
+      arr[j], arr[j + 1] = arr[j + 1], arr[j] if yield(arr[j], arr[j + 1]) == 1
     end
-    break if !swapped
   end
-
-  return array_to_sort
+  arr
 end
 
-arr_bubble_sort = [4,3,78,2,0,2];
-arr_bubble_sort_by = [4,3,78,2,0,2];
+arr_bubble_sort = [4, 3, 78, 2, 0, 2]
+arr_bubble_sort_by = [4, 3, 78, 2, 0, 2]
 
 bubble_sort(arr_bubble_sort)
 puts "bubble_sort output: #{arr_bubble_sort}"
@@ -45,7 +32,7 @@ bubble_sort_by(arr_bubble_sort_by) do |left, right|
   return_value = 0
   if left < right
     return_value = -1
-  else left > right 
+  elsif left > right
     return_value = 1
   end
   return_value
