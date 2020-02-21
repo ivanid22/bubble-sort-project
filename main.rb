3509e07e34e7d9fb1bc726af0e20ff844950e5ba
Ivan Diaz <ivanid22@gmail.com>
2020-02-21 15:15:48 -0300

    Fix bubble_sort_by to accept an array of strings

--- main.rb
+++ main.rb
@@ -14,26 +14,20 @@ def bubble_sort_by(arr = [])
     arr.each_with_index do |_, j|
       next if j >= arr.length - 1 - i
 
-      arr[j], arr[j + 1] = arr[j + 1], arr[j] if yield(arr[j], arr[j + 1]) == 1
+      arr[j], arr[j + 1] = arr[j + 1], arr[j] if yield(arr[j], arr[j + 1]) >= 1
     end
   end
   arr
 end
 
 arr_bubble_sort = [4, 3, 78, 2, 0, 2]
-arr_bubble_sort_by = [4, 3, 78, 2, 0, 2]
+arr_bubble_sort_by = %w[hi hello hey]
 
 bubble_sort(arr_bubble_sort)
 puts "bubble_sort output: #{arr_bubble_sort}"
 
-bubble_sort_by(arr_bubble_sort_by) do |left, right|
-  return_value = 0
-  if left < right
-    return_value = -1
-  elsif left > right
-    return_value = 1
-  end
-  return_value
+bubble_sort_by(arr_bubble_sort_by) do |first, second|
+  first.length - second.length
 end
 
 puts "bubble_sort_by output: #{arr_bubble_sort_by}"
